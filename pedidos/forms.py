76917abf5d16132ac78df703d59cc6c6af8a5e98
molinosdepiedra22 <molinosdepiedra22@gmail.com>
2026-03-20 14:01:11 -0600

    Mejoras UI, validaciones y cálculo de propina

--- pedidos/forms.py
+++ pedidos/forms.py
@@ -2,25 +2,108 @@ from django import forms
 from .models import Cliente, Restaurante, Repartidor, Pedido
 
 
+#cliente
 class ClienteForm(forms.ModelForm):
     class Meta:
         model  = Cliente
         fields = ['nombre', 'email', 'telefono', 'direccion', 'activo']
+        widgets = {
+            'nombre' : forms.TextInput(attrs={'class' : 'form-control'}),
+            'email' : forms.EmailInput(attrs={'class': 'form-control'}),
+            'telefono' : forms.TextInput(attrs={'class':'form-control'}),
+            'direccion' : forms.TextInput(attrs={'class':'form-control'}),
+        }
 
+    #Validaciones
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if len(nombre) < 3:
+            raise forms.ValidationError("El nombre debe tener al menos 3 caracteres")
+        return nombre
+    
+    def clean_telefono(self):
+        telefono = self.cleaned_data.get('telefono')
+
+        if not telefono.isdigit():
+            raise forms.ValidationError("El telefono solo debe contener nuúmeros")
+        
+        if len(telefono) != 10:
+            raise forms.ValidationError("El telefono debe tener 10 digitos")
+        
+        return telefono
 
 class RestauranteForm(forms.ModelForm):
     class Meta:
         model  = Restaurante
         fields = ['nombre', 'direccion', 'telefono', 'categoria', 'activo']
 
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if len(nombre) < 3:
+            raise forms.ValidationError("El nombre debe tener al menos 3 caracteres")
+        return nombre
+    
+    def clean_telefono(self):
+        telefono = self.cleaned_data.get('telefono')
+
+        if not telefono.isdigit():
+            raise forms.ValidationError("El telefono solo debe contener nuúmeros")
+        
+        if len(telefono) != 10:
+            raise forms.ValidationError("El telefono debe tener 10 digitos")
+        
+        return telefono
+
+        
+
 
 class RepartidorForm(forms.ModelForm):
     class Meta:
         model  = Repartidor
         fields = ['nombre', 'telefono', 'vehiculo', 'disponible', 'activo']
+     
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if len(nombre) < 3:
+            raise forms.ValidationError("El nombre debe tener al menos 3 caracteres")
+        return nombre
+    
+    def clean_telefono(self):
+        telefono = self.cleaned_data.get('telefono')
+
+        if not telefono.isdigit():
+            raise forms.ValidationError("El telefono solo debe contener nuúmeros")
+        
+        if len(telefono) != 10:
+            raise forms.ValidationError("El telefono debe tener 10 digitos")
+        
+        return telefono
 
 
 class PedidoForm(forms.ModelForm):
     class Meta:
         model  = Pedido
         fields = ['cliente', 'restaurante', 'repartidor', 'descripcion', 'total', 'propina', 'status']
+    
+    def clean_total(self):
+        total = self.cleaned_data.get('total')
+        if total <= 0:
+            raise forms.ValidationError("El total deve ser mayor a 0")
+        return total
+    
+    def clean_propina(self):
+        propina = self.cleaned_data.get('propina')
+
+        if not propina:
+            return '0'
+
+        try:
+            propina_int = int(propina)
+        except ValueError:
+            raise forms.ValidationError("Valor de propina inválido")
+
+        if propina_int < 0 or propina_int > 100:
+            raise forms.ValidationError("La propina debe estar entre 0 y 100%")
+
+        return propina
+    
--- pedidos/models.py
+++ pedidos/models.py
@@ -53,18 +53,20 @@ class Pedido(models.Model):
         ('entregado', 'Entregado'),
     ]
     PROPINA_CHOICES = [
-        ('0',  'Sin propina'),
-        ('10', '10%'),
-        ('15', '15%'),
-        ('20', '20%'),
-        ('25', '25%'),
+    (0,  'Sin propina'),
+    (10, '10%'),
+    (15, '15%'),
+    (20, '20%'),
+    (25, '25%'),
     ]
+
+
     cliente     = models.ForeignKey(Cliente,     on_delete=models.PROTECT)
     restaurante = models.ForeignKey(Restaurante, on_delete=models.PROTECT)
     repartidor  = models.ForeignKey(Repartidor,  on_delete=models.PROTECT, null=True, blank=True)
     descripcion = models.TextField()
     total       = models.DecimalField(max_digits=8, decimal_places=2)
-    propina     = models.CharField(max_length=2, choices=PROPINA_CHOICES, default='0')
+    propina = models.IntegerField(choices=PROPINA_CHOICES, default=0)
     status      = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pendiente')
     creado_en   = models.DateTimeField(auto_now_add=True)
     actualizado = models.DateTimeField(auto_now=True)
@@ -72,3 +74,11 @@ class Pedido(models.Model):
 
     def __str__(self):
         return f'Pedido #{self.pk} — {self.cliente}'
+    
+    def total_con_propina(self):
+        try:
+            propina = int(self.propina)
+        except:
+            propina = 0
+
+        return round(self.total + (self.total * propina / 100), 2)
